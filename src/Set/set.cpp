266c182d4fa9b8d5cae60e5dcff441bf730b11c7
Vatsal <vatsol06@gmail.com>
2018-06-03 20:30:28 -0400

    Implemented Constructors for Set()

--- src/Set/set.cpp
+++ src/Set/set.cpp
@@ -4,79 +4,138 @@
 #include <iostream>
 #include <limits.h>   // needed for INT_MIN
 
+#define SET_INVARIANT(LOC) INVARIANT_TEST(_numElements < -1, LOC)
+
 // Set up an error set to be returned as necessary
 const Set emptySet(0,(int*)0);
 const Set errorSet(-1,(int*)0);
 
 bool Set::isEmpty() const {
-  return true;
+  SET_INVARIANT("Set::isEmpty()");
+  return _numElements==0;
 }
 
 bool Set::isError() const {
-  return false;
+  SET_INVARIANT("Set::isError()");
+  return _numElements==-1;
 }
   
 int Set::cardinality() const {
+  SET_INVARIANT("Set::cardinality()");
   return 0;
 }
 
 Set Set::union_(const Set& s) const {
+  SET_INVARIANT("Set::union_()");
   return emptySet;
 }
 
 Set Set::intersection(const Set& s) const {
+  SET_INVARIANT("Set::intersection()");
   return emptySet;
 }
 
 Set Set::difference(const Set& s) const {
+  SET_INVARIANT("Set::difference()");
   return emptySet;
 }
 
 Set Set::select(predicate* p) const {
+  SET_INVARIANT("Set::select()");
   return emptySet;
 }
 
 Set Set::project(const int numItems, const int items[]) const {
+  SET_INVARIANT("Set::project()");
   return emptySet;
 }
 
 Set Set::cartesian(const Set& s) const {
+  SET_INVARIANT("Set::cartesian()");
   return emptySet;
 }
 
 Set Set::operator()(const int item) const {
+  SET_INVARIANT("Set::operator()()");
   return emptySet;
 }
 
 void Set::operator=(const Set& s) {
+  SET_INVARIANT("Set::operator=()");
 }
 
 Set::Set() {
   _numElements = 0;
+  _pTuples = NULL;
 }
 
-Set::Set(const Set& s) {
-  _numElements = s._numElements;
-  _tupleArraySize = s._tupleArraySize;
-  
-  _pTuples = new Tuples[_numElements];
-
-  for (int i=0;i<_numElements;++i){
-    _pTuples[i] = s._pTuples[i];
-  }
-
-}
+Set::Set(const Set& s) : Set(s._numElements, s._pTuples){}
 
 Set::Set(const int numElements, const int data[]) {
-  //TODO finish the constructors, finish operator<<, ???, profit
+
+  _numElements = numElements;
+
+  if (_numElements<=0){
+    _pTuples=NULL;
+    return;
+  } 
+
+  _pTuples = new Tuple[_numElements];
+
+  for (int i=0;i<_numElements;++i){
+    _pTuples[i] = Tuple(data[i]);
+  } 
 
 }
 
 Set::Set(const int numElements, const Tuple tuples[]) {
+
+  _numElements = numElements;
+
+  if (_numElements<=0){
+    _pTuples=NULL;
+    return;
+  } 
+
+  _pTuples = new Tuple[_numElements];
+
+  for (int i=0;i<numElements;++i){
+    _pTuples[i] = Tuple(tuples[i]);
+  } 
+
 }
 
 Set::~Set() {
+  delete [] _pTuples;
+  _numElements = -2; //Deleted set indication
+
 }
 
 std::ostream& operator<<(std::ostream& os, const Set& s) {
+
+  if (s._numElements==-2){
+    std::cerr << "Error: Attempted to output deleted set; exiting";
+    exit(-1);
+  }
+
+  if (s.isEmpty()){
+    os<<"()";
+    return os;
+  }
+
+  if (s.isError()){
+    os<<"(Error Set)";
+    return os;
+  }
+  
+  os<<"("<<s._pTuples[0];
+
+  for (int i=1;i<s._numElements;i++){
+    os<<", "<<s._pTuples[i];
+  }
+
+  os<<")";
+
+  return os;
+
 }
--- src/Set/test_set.cpp
+++ src/Set/test_set.cpp
@@ -24,6 +24,7 @@ bool equals(const Tuple& t) {
 }
 
 int main() {
+
   Set s1;                              // Create an empty set
   int data[] = { 222, 111, 333 };
   Set s2(3, data);                     // Create a set of three unary tuples
